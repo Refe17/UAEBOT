0a7f8302959a56aeb8d5f5a3c320aaf6e4d8d97f
AbdulazizMj <45325599+AbdulazizMj@users.noreply.github.com>
2018-12-12 08:12:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ bot.on("message", async message => {
 
 
   if(cmd === `${prefix}mute`){
-    let mRole = message.guild.roles.find("name", "Discord STAFF")
+    let mRole = message.guild.roles.find("name", "● Discord STAFF")
     if(message.member.roles.has(mRole.id)) {
     }else
     message.reply("You do not have the permission to do that.")
