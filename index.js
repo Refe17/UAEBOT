8ccfc7d2988e35718c59bc5288f70e6007d933d4
AbdulazizMj <45325599+AbdulazizMj@users.noreply.github.com>
2018-12-12 09:41:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -361,6 +361,7 @@ message.channel.send(killEmbed);
      let wUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
      if (!wUser) return message.channel.send("Please Mention a User")
      let wReason = args.join(" ").slice(22);
+    message.channel.send("**DONE!**")
    
    let warnEmbed = new Discord.RichEmbed()
    .setDescription("NEW ADMINSTRATION WARN!")
