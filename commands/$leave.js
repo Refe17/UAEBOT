71b6869aa1a261b064f852f3700e13bc9c6b72e3
AbdulazizMj <45325599+AbdulazizMj@users.noreply.github.com>
2018-12-12 14:04:51 +0300

    Update $leave.js

--- commands/$leave.js
+++ commands/$leave.js
@@ -1,8 +1,9 @@
-exports.run = (bot, message, args, ops) => {
+
+exports.run = async (bot, message, args, ops) => {
     if (!message.member.voiceChannel) return message.voiceChannel.send("Please connect to a voice channel")
 
     if(!message.guild.me.voiceChannel) return message.channel.send("Sorry, the bot isn't connected to the voice channel.");
-    if (message.guild.me.voiceChannelID !== message.me.voiceChannelID) return message.channel.send("Sorry, you aren't connected to a voice channel")
+    if (message.guild.me.voiceChannel !== message.me.voiceChannel) return message.channel.send("Sorry, you aren't connected to a voice channel")
 
     message.guild.me.voiceChannel.leave();
 
